2dfbca41d4752e2e748554e17112a6d672c7cd3c
GeorgeBodea <georgebodeacn@yahoo.com>
2021-12-09 20:10:08 +0200

    Adaugat scripturile pentru Senzor Temperatura si Display
    Am adaugat scripturile pentru:
    -senzor_temperatura: transmite temperatura catre un broker.
    -display:  se aboneaza la topicul senzorului de temperatura si afiseaza temperatura

---
+++ SistemClimatizare/display.py
@@ -0,0 +1,22 @@
+import paho.mqtt.client as client_lib
+import time
+
+broker = "mqtt.eclipseprojects.io"
+subscriber =  client_lib.Client("Display")
+
+def fnc_activa(client, user_data, message):
+  print("Mesaj primit: ", str(message.payload.decode("utf-8")), str(client))
+
+
+
+subscriber.connect(broker)
+
+subscriber.loop_start()
+
+subscriber.subscribe("Temperatura")
+subscriber.on_message = fnc_activa
+
+
+time.sleep(30)
+subscriber.loop_end()
+
---
+++ SistemClimatizare/senzor_temperatura.py
@@ -0,0 +1,14 @@
+import paho.mqtt.client as client_lib
+from random import randrange
+import time
+
+broker = "mqtt.eclipseprojects.io"
+publisher = client_lib.Client("SenzorCaldura")
+publisher.connect(broker)
+
+while True:
+  temperatura = randrange(10.0, 30.0)
+  # Primul argument din functia publish este Topicul
+  publisher.publish("Temperatura", temperatura)
+  print("S-a transmis temperatura de " + str(temperatura))
+  time.sleep(1)
