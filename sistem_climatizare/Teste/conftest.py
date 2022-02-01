bf7a4d40c14ad2d419e6e651bcedf4af1f9a8ebd
iuliaaa2110 <61266008+iuliaaa2110@users.noreply.github.com>
2022-02-01 10:21:29 +0200

    conftest.py and input_output_helper.py in Tests

---
+++ sistem_climatizare/Teste/conftest.py
@@ -0,0 +1,29 @@
+import pytest
+
+# @pytest.fixture
+def pytest_addoption(parser):
+    parser.addoption("--nume_setare", action="store", default="default name")
+    parser.addoption("--temperatura", action="store", default="0")
+    parser.addoption("--numar_persoane", action="store", default="0")
+    parser.addoption("--setare_aleasa", action="store", default="setare1")
+
+
+def pytest_generate_tests(metafunc):
+    # This is called for every test. Only get/set command line arguments
+    # if the argument is specified in the list of test "fixturenames".
+    option_value1 = metafunc.config.option.nume_setare
+    option_value2 = metafunc.config.option.temperatura
+    option_value3 = metafunc.config.option.numar_persoane
+    option_value4 = metafunc.config.option.setare_aleasa
+    
+    if 'nume_setare' in metafunc.fixturenames and option_value1 is not None:
+        metafunc.parametrize("nume_setare", [option_value1])
+
+    if 'nume_setare' in metafunc.fixturenames and option_value2 is not None:
+        metafunc.parametrize("temperatura", [option_value2])
+
+    if 'nume_setare' in metafunc.fixturenames and option_value3 is not None:
+        metafunc.parametrize("numar_persoane", [option_value3])
+
+    if 'setare_aleasa' in metafunc.fixturenames and option_value4 is not None:
+        metafunc.parametrize("setare_aleasa", [option_value4])
---
+++ sistem_climatizare/Teste/input_output_helper.py
@@ -0,0 +1,34 @@
+import builtins
+
+input_values = []
+print_values = []
+
+
+def mock_input(s):
+    global input_values, print_values
+    print_values.append(s)
+    return input_values.pop(0)
+    # return 'setare1'
+
+
+def mock_input_output_start():
+    global input_values, print_values
+
+    input_values = []
+    print_values = []
+
+    builtins.input = mock_input
+    builtins.print = lambda s: print_values.append(s)
+
+
+def get_display_output():
+    global print_values
+    return print_values
+
+
+def set_keyboard_input(mocked_inputs):
+    global input_values
+
+    mock_input_output_start()
+    input_values.extend(mocked_inputs)
+    # print(input_values)
